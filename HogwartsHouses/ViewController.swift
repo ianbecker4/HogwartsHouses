844c5ad1a3751a69428a70a38f2554898f64883e
Ian Becker <ian-becker@lambdastudents.com>
2020-07-30 16:51:14 -0700

    First commit

---
+++ HogwartsHouses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HogwartsHouses
+//
+//  Created by Ian Becker on 7/30/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
